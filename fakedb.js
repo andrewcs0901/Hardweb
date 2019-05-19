cb05d06263fc05f6ff10e6a4dbaf80f7c234e0bb
andrewcs0901 <andrewcs0901@gmail.com>
2019-05-18 22:37:27 -0300

    brincando com javaScript

---
+++ fakedb.js
@@ -0,0 +1,33 @@
+var db = {
+    noticias: [{
+        titulo: 'outras noticias 1',
+        imagem: 'https://picsum.photos/200/300',
+        resumo: '',
+        categoria: '',
+        data: ''
+    }, {
+        titulo: 'outras noticias 2',
+        imagem: 'https://picsum.photos/200/300',
+        categoria: '',
+        resumo: '',
+        data: ''
+    }, {
+        titulo: 'outras noticias 3',
+        imagem: 'https://picsum.photos/200/300',
+        categoria: '',
+        resumo: '',
+        data: ''
+    }
+    ],
+    destaque: [{
+        titulo: 'Google Stadia será mais poderoso que xbox One e ps4 juntos',
+        imagem: '',
+        categoria: '',
+        resumo:
+            'Lorem ipsum commodo ullamcorper facilisis rhoncus curabitur semper, massa fames posuere imperdiet mauris cras porttitor, id et sagittis integer ornare a. nam ultricies adipiscing molestie nunc scelerisque ipsum leo habitasse eget scelerisque sollicitudin, in donec vivamus pellentesque porta nec at nam placerat eros, a feugiat habitasse',
+        data: '',
+    }
+    ]
+
+
+}
