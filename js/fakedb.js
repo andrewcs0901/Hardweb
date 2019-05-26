3750c1eb406120342699c49f82fe0be25fb51351
andrewcs0901 <andrewcs0901@gmail.com>
2019-05-26 12:54:30 -0300

    versão 1.1 da implementação javaScript da página

--- js/fakedb.js
+++ js/fakedb.js
@@ -6,15 +6,14 @@ var db = {
             imagem: 'https://cdn-istoedinheiro-ssl.akamaized.net/wp-content/uploads/sites/17/2019/03/c.jpg',
             categoria: 'tecnologia',
             resumo:
-                'Lorem ipsum commodo ullamcorper facilisis rhoncus curabitur semper, massa fames posuere imperdiet mauris cras porttitor, id et sagittis integer ornare a. nam ultricies adipiscing molestie nunc scelerisque ipsum leo habitasse eget scelerisque sollicitudin, in donec vivamus pellentesque porta nec at nam placerat eros, a feugiat habitasse',
+                'Lorem ipsum commodo ullamcorper facilisis rhoncus curabitur semper, massa fames posuere imperdiet mauris cras porttitor, id et sagittis integer ornare a. nam ultricies adipiscing molestie nunc scelerisque ipsum leo habitasse eget scelerisque sollicitudin, in donec vivamus pellentesque porta nec at nam placerat eros, a feugiat habitasse.',
             data: '06/06/2019',
-            texto: 'lorem lorem lorem'
-        },
+            texto: '<p>O Google anunciou hoje (19) o Stadia, sua própria e aguardada plataforma de games.</p><br><p>Trata-se de um serviço de streaming que promete levar jogos de ponta, que exigem hardware avançado para rodar, para basicamente qualquer dispositivo em qualquer parte do mundo.</p><br><p>Com ele, você poderá jogar via Chrome no PC, celular, tablet e TV, por exemplo Durante a apresentação, um dado interessante foi mostrado: o Stadia tem um poder gráfico que bate os dois principais consoles do mercado, PlayStation Pro e Xbox One X No total, a Stadia possui 10,7 Teraflops na GPU, enquanto o Xbox One X fica com 6,0 Teraflops e o PlayStation 4 Pro com 4,2 Teraflops Futuramente, o Stadia chegará aos 8K com 120 fps para games</p><br>'},
         {
             id: 002,
             titulo: 'Teaser da Nvidia sugere novidades da empresa para a Computex 2019',
             imagem: 'https://picsum.photos/300/200',
-            resumo: '',
+            resumo: 'Nvidia Lorem ipsum commodo ullamcorper facilisis rhoncus curabitur semper, massa fames posuere imperdiet mauris cras porttitor, id et sagittis integer ornare a. nam ultricies adipiscing molestie nunc scelerisque ipsum leo habitasse eget scelerisque sollicitudin, in donec vivamus pellentesque porta nec at nam placerat eros, a feugiat habitasse.',
             categoria: 'tecnologia',
             data: ''
         }, {
@@ -22,14 +21,14 @@ var db = {
             titulo: 'Xiaomi anuncia Redmi 7A, smartphone mais acessível da marca',
             imagem: 'https://picsum.photos/300/200',
             categoria: 'celular',
-            resumo: '',
+            resumo: 'Xiaomi Lorem ipsum commodo ullamcorper facilisis rhoncus curabitur semper, massa fames posuere imperdiet mauris cras porttitor, id et sagittis integer ornare a. nam ultricies adipiscing molestie nunc scelerisque ipsum leo habitasse eget scelerisque sollicitudin, in donec vivamus pellentesque porta nec at nam placerat eros, a feugiat habitasse.',
             data: ''
         }, {
             id: 004,
             titulo: 'outras noticias 3',
             imagem: 'https://picsum.photos/300/200',
             categoria: '',
-            resumo: '',
+            resumo: 'Noticias variadas Lorem ipsum commodo ullamcorper facilisis rhoncus curabitur semper, massa fames posuere imperdiet mauris cras porttitor, id et sagittis integer ornare a. nam ultricies adipiscing molestie nunc scelerisque ipsum leo habitasse eget scelerisque sollicitudin, in donec vivamus pellentesque porta nec at nam placerat eros, a feugiat habitasse.',
             data: ''
         }
     ]
