0c9514cf0817133471c1341a4586a6cb08155705
andrewcs0901 <andrewcs0901@gmail.com>
2019-05-26 13:29:41 -0300

    versão 1.2 da implementação javaScript da página

--- js/fakedb.js
+++ js/fakedb.js
@@ -1,14 +1,14 @@
-var db = {
+const newLocal = {
     noticias: [
         {
             id: 001,
             titulo: 'Google Stadia será mais poderoso que xbox One e ps4 juntos',
             imagem: 'https://cdn-istoedinheiro-ssl.akamaized.net/wp-content/uploads/sites/17/2019/03/c.jpg',
             categoria: 'tecnologia',
-            resumo:
-                'Lorem ipsum commodo ullamcorper facilisis rhoncus curabitur semper, massa fames posuere imperdiet mauris cras porttitor, id et sagittis integer ornare a. nam ultricies adipiscing molestie nunc scelerisque ipsum leo habitasse eget scelerisque sollicitudin, in donec vivamus pellentesque porta nec at nam placerat eros, a feugiat habitasse.',
+            resumo: 'Lorem ipsum commodo ullamcorper facilisis rhoncus curabitur semper, massa fames posuere imperdiet mauris cras porttitor, id et sagittis integer ornare a. nam ultricies adipiscing molestie nunc scelerisque ipsum leo habitasse eget scelerisque sollicitudin, in donec vivamus pellentesque porta nec at nam placerat eros, a feugiat habitasse.',
             data: '06/06/2019',
-            texto: '<p>O Google anunciou hoje (19) o Stadia, sua própria e aguardada plataforma de games.</p><br><p>Trata-se de um serviço de streaming que promete levar jogos de ponta, que exigem hardware avançado para rodar, para basicamente qualquer dispositivo em qualquer parte do mundo.</p><br><p>Com ele, você poderá jogar via Chrome no PC, celular, tablet e TV, por exemplo Durante a apresentação, um dado interessante foi mostrado: o Stadia tem um poder gráfico que bate os dois principais consoles do mercado, PlayStation Pro e Xbox One X No total, a Stadia possui 10,7 Teraflops na GPU, enquanto o Xbox One X fica com 6,0 Teraflops e o PlayStation 4 Pro com 4,2 Teraflops Futuramente, o Stadia chegará aos 8K com 120 fps para games</p><br>'},
+            texto: '<p>O Google anunciou hoje (19) o Stadia, sua própria e aguardada plataforma de games.</p><br><p>Trata-se de um serviço de streaming que promete levar jogos de ponta, que exigem hardware avançado para rodar, para basicamente qualquer dispositivo em qualquer parte do mundo.</p><br><p>Com ele, você poderá jogar via Chrome no PC, celular, tablet e TV, por exemplo Durante a apresentação, um dado interessante foi mostrado: o Stadia tem um poder gráfico que bate os dois principais consoles do mercado, PlayStation Pro e Xbox One X No total, a Stadia possui 10,7 Teraflops na GPU, enquanto o Xbox One X fica com 6,0 Teraflops e o PlayStation 4 Pro com 4,2 Teraflops Futuramente, o Stadia chegará aos 8K com 120 fps para games</p><br>'
+        },
         {
             id: 002,
             titulo: 'Teaser da Nvidia sugere novidades da empresa para a Computex 2019',
@@ -32,6 +32,6 @@ var db = {
             data: ''
         }
     ]
-
-}
+};
+var db = newLocal
 
