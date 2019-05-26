d03f4b1b9b0a8be8250c3c9253575791c5e4d0ab
andrewcs0901 <andrewcs0901@gmail.com>
2019-05-26 10:23:52 -0300

    versão inicial do site dinâmico em javaScript

---
+++ js/fakedb.js
@@ -0,0 +1,38 @@
+var db = {
+    noticias: [
+        {
+            id: 001,
+            titulo: 'Google Stadia será mais poderoso que xbox One e ps4 juntos',
+            imagem: 'https://cdn-istoedinheiro-ssl.akamaized.net/wp-content/uploads/sites/17/2019/03/c.jpg',
+            categoria: 'tecnologia',
+            resumo:
+                'Lorem ipsum commodo ullamcorper facilisis rhoncus curabitur semper, massa fames posuere imperdiet mauris cras porttitor, id et sagittis integer ornare a. nam ultricies adipiscing molestie nunc scelerisque ipsum leo habitasse eget scelerisque sollicitudin, in donec vivamus pellentesque porta nec at nam placerat eros, a feugiat habitasse',
+            data: '06/06/2019',
+            texto: 'lorem lorem lorem'
+        },
+        {
+            id: 002,
+            titulo: 'Teaser da Nvidia sugere novidades da empresa para a Computex 2019',
+            imagem: 'https://picsum.photos/300/200',
+            resumo: '',
+            categoria: 'tecnologia',
+            data: ''
+        }, {
+            id: 003,
+            titulo: 'Xiaomi anuncia Redmi 7A, smartphone mais acessível da marca',
+            imagem: 'https://picsum.photos/300/200',
+            categoria: 'celular',
+            resumo: '',
+            data: ''
+        }, {
+            id: 004,
+            titulo: 'outras noticias 3',
+            imagem: 'https://picsum.photos/300/200',
+            categoria: '',
+            resumo: '',
+            data: ''
+        }
+    ]
+
+}
+
